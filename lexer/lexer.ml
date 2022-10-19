3cedac934fd025a43b088c37e610a37221c3fc27
yashkurkure <yash.kurkure1903@gmail.com>
2022-10-19 09:15:13 -0500

    test-07-pass1&2

--- lexer/lexer.ml
+++ lexer/lexer.ml
@@ -1,56 +1,63 @@
-let digit = [%sedlex.regexp? '0'..'9']
+let digit = [%sedlex.regexp? ('0'..'9')]
+let alphabet = [%sedlex.regexp? 'a'..'z'|'A'..'Z']
+let alphanum = [%sedlex.regexp? digit|alphabet]
 let int_constant = [%sedlex.regexp? Plus digit]
-let name = [%sedlex.regexp? "\"",Star ("\\",("n"|"t"|"\""|"\\")| Compl (Chars "\"\\\n")),"\""]
+let name = [%sedlex.regexp? alphabet, (Star alphanum)]
+
+let debug: bool = false
+
+let debug_print (s:string) =
+  if debug then (print_string s; print_newline())
 
 open Parser
 exception Eof
 
 let rec token buf =
   match%sedlex buf with
-  | "return" -> RETURN
-  | "while" -> WHILE
-  | "otherwise" -> OTHERWISE
-  | "if" -> IF
-  | "else" -> ELSE
-  | "repeat" -> REPEAT
-  | "var" -> VAR
-  | "fun" -> FUN
-  | "==" -> EQUALITY
-  | "<>" -> NOT_EQUALS
-  | '<' -> LESS_THAN
-  | '>' -> GREATER_THAN
-  | ">=" -> GREATER_THAN_EQUAL
-  | "<=" -> LESS_THAN_EQUAL
-  | "&&" -> AND
-  | "||" -> OR
-  | '!' -> NOT
-  | '^' -> XOR
-  | '&' -> BINARY_AND
-  | '|' -> BINARY_OR
-  | '+' -> ADD
-  | '%' -> REMAINDER
-  | '-' -> SUB
-  | '*' -> MULTIPLY
-  | '/' -> DIVISION
-  | "int" -> TYPE
-  | "string" -> TYPE
-  | "char" -> TYPE
-  | "void" -> TYPE
-  | name -> NAME (Sedlexing.Latin1.lexeme buf)
-  | '(' -> LEFT_PARENTHESIS
-  | ')' -> RIGHT_PARENTHESIS
-  | '{' -> LEFT_CURLY
-  | '}' -> RIGHT_CURLY
-  | '[' -> LEFT_BRACKET
-  | ']' -> RIGHT_BRACKET
-  | ';' -> SEMICOLON
-  | ":=" -> ASSIGNMENT
-  | ',' -> COMMA
+  | "return" -> debug_print "RETURN"; RETURN
+  | "while" -> debug_print "WHILE";WHILE
+  | "otherwise" -> debug_print "RETURN";OTHERWISE
+  | "if" -> debug_print "IF";IF
+  | "else" -> debug_print "RETURN";ELSE
+  | "repeat" -> debug_print "REPEAT";REPEAT
+  | "var" -> debug_print "RETURN";VAR
+  | "fun" -> debug_print "RETURN";FUN
+  | "==" -> debug_print "RETURN";EQUALITY
+  | "<>" -> debug_print "RETURN";NOT_EQUALS
+  | '<' -> debug_print "RETURN";LESS_THAN
+  | '>' -> debug_print "RETURN";GREATER_THAN
+  | ">=" -> debug_print "RETURN";GREATER_THAN_EQUAL
+  | "<=" -> debug_print "RETURN";LESS_THAN_EQUAL
+  | "&&" -> debug_print "RETURN";AND
+  | "||" -> debug_print "RETURN";OR
+  | '!' -> debug_print "RETURN";NOT
+  | '^' -> debug_print "RETURN";XOR
+  | '&' -> debug_print "RETURN";BINARY_AND
+  | '|' -> debug_print "RETURN";BINARY_OR
+  | '+' -> debug_print "RETURN";ADD
+  | '%' -> debug_print "RETURN";REMAINDER
+  | '-' -> debug_print "RETURN";SUB
+  | '*' -> debug_print "RETURN";MULTIPLY
+  | '/' -> debug_print "RETURN";DIVISION
+  | "int" -> debug_print "RETURN";TYPE
+  | "string" -> debug_print "RETURN";TYPE
+  | "char" -> debug_print "RETURN";TYPE
+  | "void" -> debug_print "RETURN";TYPE
+  | name -> debug_print "NAME";NAME (Sedlexing.Latin1.lexeme buf)
+  | int_constant -> debug_print "RETURN"; INT_CONSTANT (Sedlexing.Latin1.lexeme buf)
+  | '(' -> debug_print "LPAREN";LEFT_PARENTHESIS
+  | ')' -> debug_print "RPAREN";RIGHT_PARENTHESIS
+  | '{' -> debug_print "LCURLY";LEFT_CURLY
+  | '}' -> debug_print "RCURLY";RIGHT_CURLY
+  | '[' -> debug_print "RETURN";LEFT_BRACKET
+  | ']' -> debug_print "RETURN";RIGHT_BRACKET
+  | ';' -> debug_print "SEMICOLON";SEMICOLON
+  | ":=" -> debug_print "ASSIGNMENT";ASSIGNMENT
+  | ',' -> debug_print "COMMA";COMMA
   | '\n' -> token buf
   | Plus (Chars " \t") -> token buf
-  | int_constant -> INT_CONSTANT (Sedlexing.Latin1.lexeme buf)
   | eof -> raise Eof
-  | _ -> failwith "Unexpected character"
+  | _ -> debug_print (Sedlexing.Latin1.lexeme buf); failwith "Unexpected character"
 
 
 (* let rec token buf =
