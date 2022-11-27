a89cea6793fd19e0e6e8c91398dc89a1d711a118
yashkurkure <yash.kurkure1903@gmail.com>
2022-11-27 10:27:31 -0600

    [CHCKPNT] test 02-stmts pass

--- semantic_analysis/type_checks.ml
+++ semantic_analysis/type_checks.ml
@@ -16,16 +16,34 @@ let rec type_check_exprs (exprs: exp_node list)(f: fun_frame option) =
   | [], _ -> assert(true);;
 
 
-let type_check_stmt (stmt: stmt_node)(f: fun_frame option) = 
+let rec type_check_stmt (stmt: stmt_node)(f: fun_frame option) = 
   match stmt, f with
   | Return(exp), f -> (match (type_check_expr exp f), f with
-                    | Some(String_ty), None -> exit(3) (* Cannot return string type from top level, only int is allowed *)
+                    | Some(String_ty), None -> exit(3) (* Fail when top level returns String_ty *)
                     | Some(Int_ty), None -> assert(true)
                     | Some(t1), Some(FunFrame(FunDec(_,t2,_,_,_))) -> if t1 = t2 then assert(true) else exit(3)
                     | _, _ -> assert(false))
-  | _, _ -> assert(false);;
 
-let rec type_check_stmts (stmts: stmt_node list)(f: fun_frame option): unit = 
+  | IfThenElse(cond, thn, els), _ -> (match (type_check_expr cond f) with 
+                                     | Some(Int_ty) -> assert(true)
+                                     | _ -> exit(3));  (* Fail when guard is not Int_ty *)
+                                     ignore(type_check_stmts(thn)(f));
+                                     ignore(type_check_stmts(els)(f))
+
+  | WhileOtherwise(cond, body, otherwise), _ -> (match (type_check_expr cond f) with 
+                                                | Some(Int_ty) -> assert(true)
+                                                | _ -> exit(3));  (* Fail when guard is not Int_ty *)
+                                                ignore(type_check_stmts(body)(f));
+                                                ignore(type_check_stmts(otherwise)(f))
+
+  | Repeat(times, body), _ -> (match (type_check_expr times f) with 
+                              | Some(Int_ty) -> assert(true)
+                              | _ -> exit(3));  (* Fail when times is not Int_ty *)
+                              ignore(type_check_stmts(body)(f))
+
+  | _, _ -> assert(false)
+
+and type_check_stmts (stmts: stmt_node list)(f: fun_frame option): unit = 
   match stmts, f with
   | stmt::rest, f -> ignore(type_check_stmt(stmt)(f)); type_check_stmts(rest)(f)
   | [], _ -> assert(true);;
