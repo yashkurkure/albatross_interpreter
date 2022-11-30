3512ee0f7fd53418995302c4e1959f56204a18eb
Yash Kurkure <61632337+yashkurkure@users.noreply.github.com>
2022-11-30 00:39:43 -0600

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -105,6 +105,6 @@ then
 else
     echo "Finished running tests"
     echo "Setting envrionment variables with test results.."
-    echo "{PASSING_TESTS}={$PASSING}" >> $GITHUB_ENV
-    echo "{TOTAL_TESTS}={$TOTAL_TESTS}" >> $GITHUB_ENV
+    echo "PASSING_TESTS=$PASSING" >> $GITHUB_ENV
+    echo "TOTAL_TESTS=$TOTAL_TESTS" >> $GITHUB_ENV
 fi
