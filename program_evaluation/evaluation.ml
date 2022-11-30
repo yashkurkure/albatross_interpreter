33aa02737e7d566a0a61332b9d9f0581b71ba3af
yashkurkure <yash.kurkure1903@gmail.com>
2022-11-29 18:34:28 -0600

    [CHCKPNT] test 06-global-ints pass

--- program_evaluation/evaluation.ml
+++ program_evaluation/evaluation.ml
@@ -4,7 +4,10 @@ open Global_table
 
 let rec eval_expr (e: exp_node) (globals: globtab) (c: context): albatross_type = 
   match e, c with
-  | Ident(_), _ -> assert(false)
+  | Ident(x), Glob_ct -> (match lookup_globtab globals x with
+                          | Some(vt) -> vt 
+                          | None -> exit(3))
+  | Ident(_), Func_ct(_) -> assert(false)
   | Add(e1, e2), c -> (match eval_expr e1 globals c, eval_expr e2 globals c with
                     | Int(v1), Int(v2) -> Int(v1+v2)
                     | _,_ -> exit(3))
@@ -61,27 +64,30 @@ let rec eval_expr (e: exp_node) (globals: globtab) (c: context): albatross_type 
   | FunCallExp(_, _), _  -> assert(false)
   | Nil, _  -> Void;;
 
-(* let rec init_global_vars (vars: vardec_node list) (globals: globtab): globtab = 
+let rec init_global_vars (vars: vardec_node list) (globals: globtab): globtab = 
   match vars with 
-  | VarDec(x, _, e)::rest -> init_global_vars rest (update_globtab globals x (eval_expr e globals))
+  | VarDec(x, _, e)::rest -> init_global_vars rest (update_globtab globals x (eval_expr e globals Glob_ct))
   | [] -> globals
 
-let init_global_funcs (_: fundec_node list) (_: globtab): globtab = empty_globtab *)
+let init_global_funcs (_: fundec_node list) (_: globtab): globtab = empty_globtab
 
-let rec eval_stmts (stmts: stmt_node list)(globals: globtab)(c: context): unit =
+let rec eval_stmts (stmts: stmt_node list)(globals: globtab)(c: context): globtab =
   match stmts with
-  | stmt::rest -> eval_stmt stmt globals c; eval_stmts rest globals c
-  | [] -> print_string ""
-and eval_stmt (stmt: stmt_node)(globals: globtab)(c: context): unit = 
+  | stmt::rest -> eval_stmts rest (eval_stmt stmt globals c) c
+  | [] -> globals
+and [@warning "-21"] eval_stmt (stmt: stmt_node)(globals: globtab)(c: context): globtab = 
   match stmt, c with
-  | Return(e), Glob_ct -> print_newline() ;exit(match eval_expr e globals c with Int(v) -> v | Void -> 0| String(_) -> exit(3) | Function(_)-> exit(3))
-  | FunCallStmt(x,e::[]), c when x = "exit"-> print_newline() ;exit(match eval_expr e globals c with Int(v) -> v | Void -> 0| String(_) -> exit(3) | Function(_)-> exit(3))
-  | FunCallStmt(x, e::[]), c when x = "printint" -> print_int(match eval_expr e globals c with Int(v) -> v | Void -> 0| String(_) -> exit(3) | Function(_)-> exit(3))
+  | Return(e), Glob_ct -> print_newline() ;exit(match eval_expr e globals c with Int(v) -> v | Void -> 0| String(_) -> exit(3) | Function(_)-> exit(3)); globals
+  | FunCallStmt(x,e::[]), c when x = "exit"-> print_newline() ;exit(match eval_expr e globals c with Int(v) -> v | Void -> 0| String(_) -> exit(3) | Function(_)-> exit(3)); globals
+  | FunCallStmt(x, e::[]), c when x = "printint" -> print_int(match eval_expr e globals c with Int(v) -> v | Void -> 0| String(_) -> exit(3) | Function(_)-> exit(3)); globals
   | IfThenElse(e, thn, el), c -> (match eval_expr e globals c with 
                               | Int(v) -> if v!=0 then eval_stmts thn globals c else eval_stmts el globals c 
                               | _ -> exit(3))
+  | Assign(x, e), Glob_ct-> (match eval_expr e globals c with
+                            | vt -> update_globtab globals x vt)
+  | Assign(_,_), Func_ct(_) -> assert(false) 
   | _, _ -> assert(false)
 
-let eval (p: program) = 
+let [@warning "-5"] eval (p: program) = 
   match p with
-  | Program(_,_,stmts) -> eval_stmts stmts empty_globtab Glob_ct
+  | Program(vars,_,stmts) -> ignore(eval_stmts stmts (init_global_vars vars empty_globtab) Glob_ct)
